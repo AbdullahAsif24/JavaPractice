dded47109af3d75c5c65d0c2b47ba376e1ae27f4
AbdullahAsif24 <abdullahasif4045@gmail.com>
2025-05-23 11:35:28 +0500

    Refactor Question12 to sort records by sales amount and update main method signature; add BinarySearch implementation

--- FilesInJava/operations-on-data/Question12.java
+++ FilesInJava/operations-on-data/Question12.java
@@ -1,36 +1,52 @@
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
 public class Question12 {
     // QUESTION 12
     // Write java code that prints all the record sorted in descending order with
     // respect to sales amount
-    public static void main() {
+    public static void main(String[] args) {
         try {
             String myFilePath = "E:\\UBIT\\programming fundamentals 1st sem\\java\\JavaPractice\\FilesInJava\\operations-on-data\\sampleForQuizExam.txt";
             String line = "";
+            String header = "";
             String[] fields;
-            ArrayList<Integer> ammount = new ArrayList<>();
+            List<String[]> records = new ArrayList<>();
 
             File myFile = new File(myFilePath);
             Scanner fileToRead = new Scanner(myFile);
 
-            line = fileToRead.nextLine();
+            header = fileToRead.nextLine();
             while (fileToRead.hasNext()) {
                 line = fileToRead.nextLine();
                 fields = line.split("\t");
 
-                int qty = Integer.parseInt(fields[3]);
-                int unitPrice = Integer.parseInt(fields[2]);
-                ammount.add( qty * unitPrice) ;
+                records.add(fields);
+            }
+            fileToRead.close();
+
+            records.sort((a, b)->{
+                int AmmountA = Integer.parseInt(a[2]);
+                int qtyA = Integer.parseInt(a[3]);
+                int sellPriceA = AmmountA*qtyA;
+                
+                int AmmountB = Integer.parseInt(b[2]);
+                int qtyB = Integer.parseInt(b[3]);
+                int sellPriceB = AmmountB*qtyB;
+
+                return Integer.compare(sellPriceB, sellPriceA);
+            });
+
+            System.out.println(header);
+
+            for (int i = 0; i < records.size(); i++) {
+                String[] record = records.get(i);
+                System.out.println(String.join("\t", record));
             }
 
-            // sort ammount array in desc order
-            Collections.sort(ammount, Collections.reverseOrder());
-            
-            System.out.println(ammount);
         } catch (Exception e) {
             System.out.println(e);
         }
